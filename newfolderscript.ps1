63d501d10ff2c704539bba70f23bfa53606104fa
saddmin <55796126+saddmin@users.noreply.github.com>
2020-06-12 15:36:46 -0400

    Add files via upload

---
+++ newfolderscript.ps1
@@ -0,0 +1,9 @@
+﻿
+    {
+        New-Item -Path $env\scriptoutputs -ItemType "directory"
+
+        Set-Content -Path $env\scriptoutputs\created.txt -Value "this directory was created"
+
+        Get-Date -Format g | 
+        Add-Content -Path $env\scriptoutputs\created.txt
+    }leNameChecking -allowclobber
---
+++ timedshutdown.ps1
@@ -0,0 +1,7 @@
+﻿  
+#stop script for a specified time, then execute script. 
+do {
+Start-Sleep 1
+}
+until ((get-date) -ge (get-date "6:00 PM"))
+Restart-Computer
