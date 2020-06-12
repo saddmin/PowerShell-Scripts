1a6f07fcd0a253ffef0a1a8e2ff540c522bb93b8
saddmin <55796126+saddmin@users.noreply.github.com>
2020-06-12 15:55:04 -0400

    Add files via upload

---
+++ domaincomputerOSsort.ps1
@@ -0,0 +1,58 @@
+﻿#get all computers on domain and sort by OS
+{
+
+Get-ADComputer -Filter 'operatingsystem -notlike "*server*" -and enabled -eq "true"' `
+-Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
+Sort-Object -Property Operatingsystem |
+Select-Object -Property Name,Operatingsystem,OperatingSystemVersion,IPv4Address
+
+}
+
+#get all server on domain and sort by OS
+{
+
+Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' `
+-Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
+Sort-Object -Property Operatingsystem |
+Select-Object -Property Name,Operatingsystem,OperatingSystemVersion,IPv4Address
+
+}
+
+# get all domain controllers and sort by OS
+
+{
+
+Get-ADComputer -Filter 'primarygroupid -eq "516"' `
+-Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
+Sort-Object -Property Operatingsystem |
+Select-Object -Property Name,Operatingsystem,OperatingSystemVersion,IPv4Address
+
+}
+
+#get all servers that are not domain controllers and sort by OS
+
+{
+
+Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true" -and primarygroupid -ne "516"' `
+-Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
+Sort-Object -Property Operatingsystem |
+Select-Object -Property Name,Operatingsystem,OperatingSystemVersion,IPv4Address
+
+}
+
+
+#get all domain computers sorted by OS
+
+{
+
+Get-ADComputer -Filter 'enabled -eq "true"' `
+-Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
+Sort-Object -Property Operatingsystem |
+Select-Object -Property Name,Operatingsystem,OperatingSystemVersion,IPv4Address
+
+}
+
+
+#get all domain computers with service version
+Get-ADComputer -Filter * -Property * | 
+Format-Table Name,OperatingSystem,OperatingSystemServicePack,OperatingSystemVersion -Wrap –Auto
